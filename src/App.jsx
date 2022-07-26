ca96fce07d374fa7d57059c155b2a90048c663ee
Edgar Egidio <37923860+edgaregidio@users.noreply.github.com>
2022-07-26 16:14:15 -0300

    new footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 import Nav from './components/core/Nav'
 import Foods from './components/common/Foods'
 import Drinks from './components/common/Drinks'
-import SafeArea from 'react-safe-area-component'
+import Footer from './components/common/Footer'
 
 import './global.css'
 
 function App() {
   return (
     <>
-      <SafeArea top bottom>
-        <Nav />
-      </SafeArea>
+      <Nav />
       <Foods />
       <Drinks />
+      <Footer />
     </>
   )
 }
---
+++ src/components/common/Footer/index.jsx
@@ -0,0 +1,51 @@
+import { Icon } from '@iconify/react'
+import logoCircle from '../../../assets/logoCircle.png'
+
+import './styles.css'
+
+function Footer() {
+  return (
+    <>
+      <section className="footer">
+        <div className="col-footer">
+          <img src={logoCircle} alt="Logomarca Canada Padaria e Comedoria" />
+        </div>
+        <div className="col-footer">
+          <h1>Contato</h1>
+
+          <a href="https://google.com" target="_blank" className="link-contact">
+            <div className="contact">
+              <Icon
+                icon="ant-design:instagram-filled"
+                width="32"
+                className="icon-footer"
+              />
+              <h2>@panificadoracanada</h2>
+            </div>
+          </a>
+
+          <a href="https://google.com" target="_blank" className="link-contact">
+            <div className="contact">
+              <Icon
+                icon="dashicons:whatsapp"
+                width="32"
+                className="icon-footer"
+              />
+              <h2>(62) 3214-2586</h2>
+            </div>
+          </a>
+        </div>
+      </section>
+      {/* <div className="content-dev">
+        <h1>
+          Desenvolvido por{' '}
+          <a href="https://github.com/edgaregidio" target="_blank">
+            Edgar Egidio
+          </a>
+        </h1>
+      </div> */}
+    </>
+  )
+}
+
+export default Footer
