76aa6a68a3a8790e6ace20ac1172173e05caa189
Edgar Egidio <37923860+edgaregidio@users.noreply.github.com>
2022-07-26 17:16:38 -0300

    add ScrollToTop

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Nav from './components/core/Nav'
 import Foods from './components/common/Foods'
 import Drinks from './components/common/Drinks'
 import Footer from './components/common/Footer'
+import ScrollToTop from 'react-scroll-to-top'
 
 import './global.css'
 
@@ -9,6 +10,13 @@ function App() {
   return (
     <>
       <Nav />
+      <ScrollToTop
+        smooth
+        viewBox="0 0 256 256"
+        className="btnUp"
+        width="22"
+        color="#bc0623"
+      />
       <Foods />
       <Drinks />
       <Footer />
