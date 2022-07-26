2b97cf755238275a50076f1b369f0be1778f4186
Edgar Egidio <37923860+edgaregidio@users.noreply.github.com>
2022-07-26 12:32:19 -0300

    new list food

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import './global.css'
 import Nav from './components/core/Nav'
+import Foods from './components/common/Foods'
+
+import './global.css'
 
 function App() {
   return (
     <>
       <Nav />
+      <Foods />
     </>
   )
 }
