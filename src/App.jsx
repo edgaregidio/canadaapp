9afd66c3d0997b919bf3ab16aa23cf1399dec0c1
Edgar Egidio <37923860+edgaregidio@users.noreply.github.com>
2022-07-28 14:49:31 -0300

    correction page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import Nav from './components/core/Nav'
-import Foods from './components/common/Foods'
-import Drinks from './components/common/Drinks'
-import Footer from './components/common/Footer'
-import ScrollToTop from 'react-scroll-to-top'
+// import Nav from './components/core/Nav'
+// import Foods from './components/common/Foods'
+// import Drinks from './components/common/Drinks'
+// import Footer from './components/common/Footer'
+// import ScrollToTop from 'react-scroll-to-top'
+
+import { Icon } from '@iconify/react'
 
 import './global.css'
 
 function App() {
   return (
     <>
-      <Nav />
+      {/* <Nav />
       <ScrollToTop
         smooth
         viewBox="0 0 256 256"
@@ -19,7 +21,10 @@ function App() {
       />
       <Foods />
       <Drinks />
-      <Footer />
+      <Footer /> */}
+      <p style={{ position: 'absolute', left: '45%', top: '45%' }}>
+        Aguarde... <Icon icon="eos-icons:loading" />{' '}
+      </p>
     </>
   )
 }
