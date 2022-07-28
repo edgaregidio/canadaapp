83915012ac516983974c000bc3a284d0978c498b
Edgar Egidio <37923860+edgaregidio@users.noreply.github.com>
2022-07-28 15:33:51 -0300

    page started

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-// import Nav from './components/core/Nav'
-// import Foods from './components/common/Foods'
-// import Drinks from './components/common/Drinks'
-// import Footer from './components/common/Footer'
-// import ScrollToTop from 'react-scroll-to-top'
+import Nav from './components/core/Nav'
+import Foods from './components/common/Foods'
+import Drinks from './components/common/Drinks'
+import Footer from './components/common/Footer'
+import ScrollToTop from 'react-scroll-to-top'
 
 import { Icon } from '@iconify/react'
 
@@ -11,7 +11,7 @@ import './global.css'
 function App() {
   return (
     <>
-      {/* <Nav />
+      <Nav />
       <ScrollToTop
         smooth
         viewBox="0 0 256 256"
@@ -21,8 +21,8 @@ function App() {
       />
       <Foods />
       <Drinks />
-      <Footer /> */}
-      <span
+      <Footer />
+      {/* <span
         style={{
           position: 'absolute',
           left: '30%',
@@ -34,7 +34,7 @@ function App() {
         <span style={{ position: 'relative', left: '3.5rem', top: '2rem' }}>
           <Icon icon="eos-icons:loading" />
         </span>
-      </span>
+      </span> */}
     </>
   )
 }
